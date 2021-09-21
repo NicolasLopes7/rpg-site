15e0e6ee367fe1c8f88e6968bbc041ff9ec76453
Antonio Ricardo de Oliveira Filho <a.pedro.neto.antonio@gmail.com>
2021-09-20 22:08:45 -0300

    mudando o console.log

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const app = express()
 const adminRoutes = require("./rotas/admin")
 const bodyParser = require("body-parser")
+const porta = 3333
 
 app.set('view engine',  'ejs')
 
@@ -13,6 +14,6 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(adminRoutes)
 
-app.listen(3333, () => {
-    console.log("ta rodando")
+app.listen(porta, () => {
+    console.log(`ta rodando na porta: ${porta}`)
 })
